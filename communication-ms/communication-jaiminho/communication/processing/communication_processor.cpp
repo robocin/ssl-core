d568c7ba0e858e9ff11274fa688ea795f0387fe0
Elisson Araújo <44985807+ersaraujo@users.noreply.github.com>
2024-10-17 14:39:31 -0300

    Add processing classes to communication (#26)
    This pull request add processing classes to communication service.

---
+++ communication-ms/communication-jaiminho/communication/processing/communication_processor.cpp
@@ -0,0 +1,64 @@
+#include "communication/processing/communication_processor.h"
+
+#include "communication/messaging/receiver/payload.h"
+
+#include <protocols/communication/robot_info.pb.h>
+#include <protocols/navigation/navigation.pb.h>
+
+#include <ranges>
+
+namespace communication {
+
+namespace parameters = ::robocin::parameters;
+
+namespace {
+
+namespace rc {
+
+using ::protocols::communication::RobotComm;
+using ::protocols::navigation::Navigation;
+
+} // namespace rc
+
+namespace tp {
+
+using ::protocols::third_party::game_controller::Referee;
+
+} // namespace tp
+
+std::vector<rc::Navigation> navigationFromPayloads(std::span<const Payload> payloads) {
+    return payloads | std::views::transform(&Payload::getNavigation) | std::views::join
+            | std::ranges::to<std::vector>();
+}
+
+std::vector<tp::Referee> refereeFromPayloads(std::span<const Payload> payloads) {
+    return payloads | std::views::transform(&Payload::getReferee) | std::views::join
+            | std::ranges::to<std::vector>();
+
+}
+
+} // namespace
+
+CommunicationProcessor::CommunicationProcessor(
+    std::unique_ptr<parameters::IHandlerEngine> parameters_handler_engine,
+    std::unique_ptr<IRobotCommandMapper> robot_command_mapper):
+    parameters_handler_engine_{std::move(parameters_handler_engine)},
+    robot_command_mapper_{std::move(robot_command_mapper)} {}
+
+std::optional<rc::RobotComm> CommunicationProcessor::process(std::span<const Payload> payloads) {
+
+    if(std::vector<tp::Referee> referees = refereeFromPayloads(payloads); !referees.empty()) {
+        last_game_controller_referee_ = std::move(referees.back());
+    }
+
+    std::vector<rc::Navigation> navigation = navigationFromPayloads(payloads);
+    if(navigation.empty()) {
+        return std::nullopt;
+    }
+
+    const rc::Navigation last_navigation = navigation.back();
+
+    return robot_command_mapper_->fromNavigationAndReferee(last_navigation, *last_game_controller_referee_);
+}
+
+} // namespace communication
---
+++ communication-ms/communication-jaiminho/communication/processing/robot_command/robot_command_mapper.cpp
@@ -0,0 +1,55 @@
+#include "communication/processing/robot_command/robot_command_mapper.h"
+
+#include <google/protobuf/util/time_util.h>
+#include <protocols/navigation/navigation.pb.h>
+#include <protocols/communication/robot_info.pb.h> 
+#include <protocols/third_party/game_controller/referee.pb.h>
+#include <robocin/memory/object_ptr.h>
+#include <utility>
+
+namespace communication {
+namespace {
+
+using ::google::protobuf::util::TimeUtil;
+using ::robocin::object_ptr;
+
+namespace rc {
+    
+using ::protocols::navigation::Navigation;
+using ::protocols::communication::RobotComm;
+using ::protocols::communication::Command;
+
+} // namespace rc
+
+namespace tp {
+
+using ::protocols::third_party::game_controller::Referee;
+
+} // namespace tp
+
+} // namespace
+
+RobotCommandMapper::RobotCommandMapper(){}
+
+rc::RobotComm RobotCommandMapper::fromNavigationAndReferee(const rc::Navigation& navigation, const tp::Referee& referee) {
+    rc::RobotComm result;
+    rc::Command* command = result.mutable_command();
+
+    // *command->mutable_msg_type(); // TODO
+
+    // *command->mutable_robot_id(); // TODO
+
+    // *command->mutable_ref_command(); // TODO
+
+    // *command->mutable_robot_velocity(); // TODO
+
+    // *command->mutable_kick_command(); // TODO
+
+    // *command->mutable_dribble_command(); // TODO
+
+    // *command->mutable_robot_flag(); // TODO
+
+    return result;
+}
+
+} // namespace communication
