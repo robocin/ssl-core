53461c1ccaa7335c5edf3a02af74a720ef295aec
fnalmeidap <fnap@cin.ufpe.br>
2024-10-24 09:37:27 -0300

    Add interface

---
+++ decision-ms/decision-guardiola/decision/processing/messages/iproto_convertible.h
@@ -0,0 +1,23 @@
+#ifndef DECISION_MESSAGES_IPROTO_CONVERTIBLE
+#define DECISION_MESSAGES_IPROTO_CONVERTIBLE
+
+namespace decision {
+
+template <typename T>
+class IProtoConvertible {
+ public:
+  IProtoConvertible() = default;
+
+  IProtoConvertible(const IProtoConvertible&) = delete;
+  IProtoConvertible& operator=(const IProtoConvertible&) = delete;
+  IProtoConvertible(IProtoConvertible&&) noexcept = default;
+  IProtoConvertible& operator=(IProtoConvertible&&) noexcept = default;
+
+  virtual ~IProtoConvertible() = default;
+
+  virtual T toProto() const = 0;
+};
+
+} // namespace decision
+
+#endif // DECISION_MESSAGES_IPROTO_CONVERTIBLE
