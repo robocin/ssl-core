1c888426052a9e060d7a229b241163296a2b02e2
ersaraujo <ersa@cin.ufpe.br>
2024-10-27 23:06:01 -0300

    Refactor RobotVelocity message implementation to cpp file

--- communication-ms/communication-jaiminho/communication/processing/messages/common/robot_velocity/robot_velocity.cpp
+++ communication-ms/communication-jaiminho/communication/processing/messages/common/robot_velocity/robot_velocity.cpp
@@ -1,10 +1,33 @@
-#include "communication/processing/messages/common/robot_velocity/robot_velocity.h"
+#include "robot_velocity.h"
 
 namespace communication {
 
 RobotVelocityMessage::RobotVelocityMessage(std::optional<robocin::Point2D<float>> velocity,
                                            std::optional<float> angular_velocity) :
-    velocity{std::move(velocity)},
-    angular_velocity{std::move(angular_velocity)} {}
+    velocity(std::move(velocity)),
+    angular_velocity(std::move(angular_velocity)) {}
+
+protocols::common::RobotVelocity RobotVelocityMessage::toProto() const {
+  protocols::common::RobotVelocity robot_velocity;
+  protocols::common::Point2Df point2Df;
+  if (velocity.has_value()) {
+    point2Df.set_x(velocity.value().x);
+    point2Df.set_y(velocity.value().y);
+    robot_velocity.mutable_velocity()->CopyFrom(point2Df);
+  }
+  robot_velocity.set_angular_velocity(angular_velocity.value_or(0));
+
+  return robot_velocity;
+}
+
+void RobotVelocityMessage::fromProto(protocols::common::RobotVelocity robot_velocity) {
+  if (robot_velocity.has_velocity()) {
+    robocin::Point2D<float> point2Df;
+    point2Df.x = robot_velocity.velocity().x();
+    point2Df.y = robot_velocity.velocity().y();
+    velocity = point2Df;
+  }
+  angular_velocity = robot_velocity.angular_velocity();
+}
 
 } // namespace communication
--- communication-ms/communication-jaiminho/communication/processing/messages/common/robot_velocity/robot_velocity.h
+++ communication-ms/communication-jaiminho/communication/processing/messages/common/robot_velocity/robot_velocity.h
@@ -1,6 +1,7 @@
-#ifndef COMMUNICATION_PROCESSING_MESSAGES_COMMON_ROBOT_VELOCITY_ROBOT_KICK_H
-#define COMMUNICATION_PROCESSING_MESSAGES_COMMON_ROBOT_VELOCITY_ROBOT_KICK_H
+#ifndef COMMUNICATION_PROCESSING_MESSAGES_COMMON_ROBOT_VELOCITY_ROBOT_VELOCITY_H
+#define COMMUNICATION_PROCESSING_MESSAGES_COMMON_ROBOT_VELOCITY_ROBOT_VELOCITY_H
 
+#include <optional>
 #include <protocols/common/geometry.pb.h>
 #include <protocols/common/robot_velocity.pb.h>
 #include <robocin/geometry/point2d.h>
@@ -13,28 +14,9 @@ class RobotVelocityMessage : public robocin::IProtoConvertible<protocols::common
   explicit RobotVelocityMessage(std::optional<robocin::Point2D<float>> velocity = std::nullopt,
                                 std::optional<float> angular_velocity = std::nullopt);
 
-  [[nodiscard]] protocols::common::RobotVelocity toProto() const override {
-    protocols::common::RobotVelocity robot_velocity;
-    protocols::common::Point2Df point2Df;
-    if (velocity.has_value()) {
-      point2Df.set_x(velocity.value().x);
-      point2Df.set_y(velocity.value().y);
-      robot_velocity.mutable_velocity()->CopyFrom(point2Df);
-    }
-    robot_velocity.set_angular_velocity(angular_velocity.value_or(0));
+  [[nodiscard]] protocols::common::RobotVelocity toProto() const override;
 
-    return robot_velocity;
-  };
-
-  void fromProto(protocols::common::RobotVelocity robot_velocity) override {
-    if (robot_velocity.has_velocity()) {
-      robocin::Point2D<float> point2Df;
-      point2Df.x = robot_velocity.velocity().x();
-      point2Df.y = robot_velocity.velocity().y();
-      velocity = point2Df;
-    }
-    angular_velocity = robot_velocity.angular_velocity();
-  };
+  void fromProto(protocols::common::RobotVelocity robot_velocity) override;
 
   std::optional<robocin::Point2D<float>> velocity;
   std::optional<float> angular_velocity;
@@ -42,4 +24,4 @@ class RobotVelocityMessage : public robocin::IProtoConvertible<protocols::common
 
 } // namespace communication
 
-#endif // COMMUNICATION_PROCESSING_MESSAGES_COMMON_ROBOT_VELOCITY_ROBOT_KICK_H
+#endif // COMMUNICATION_PROCESSING_MESSAGES_COMMON_ROBOT_VELOCITY_ROBOT_VELOCITY_H
