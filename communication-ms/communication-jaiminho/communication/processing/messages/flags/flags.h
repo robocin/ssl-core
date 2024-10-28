9d13c8c646c06503c40f0733675a9039b437a6b9
ersaraujo <ersa@cin.ufpe.br>
2024-10-27 23:05:38 -0300

    Refactor Flags message implementation to cpp file

--- communication-ms/communication-jaiminho/communication/processing/messages/flags/flags.h
+++ communication-ms/communication-jaiminho/communication/processing/messages/flags/flags.h
@@ -1,6 +1,7 @@
 #ifndef COMMUNICATION_PROCESSING_MESSAGES_FLAGS_H
 #define COMMUNICATION_PROCESSING_MESSAGES_FLAGS_H
 
+#include <optional>
 #include <protocols/communication/robot_info.pb.h>
 #include <robocin/utility/iproto_convertible.h>
 
@@ -8,24 +9,13 @@ namespace communication {
 
 class FlagsMessage : public robocin::IProtoConvertible<protocols::communication::Flags> {
  public:
-  explicit FlagsMessage(std::optional<bool> robot_locked = std::nullopt,
-                        std::optional<bool> critical_move = std::nullopt,
-                        std::optional<bool> global_speed = std::nullopt);
+  FlagsMessage(std::optional<bool> robot_locked = std::nullopt,
+               std::optional<bool> critical_move = std::nullopt,
+               std::optional<bool> global_speed = std::nullopt);
 
-  [[nodiscard]] protocols::communication::Flags toProto() const override {
-    protocols::communication::Flags flags;
-    flags.set_robot_locked(robot_locked.value_or(false));
-    flags.set_critical_move(critical_move.value_or(false));
-    flags.set_global_speed(global_speed.value_or(false));
+  [[nodiscard]] protocols::communication::Flags toProto() const override;
 
-    return flags;
-  };
-
-  void fromProto(protocols::communication::Flags flags) override {
-    robot_locked = flags.robot_locked();
-    critical_move = flags.critical_move();
-    global_speed = flags.global_speed();
-  };
+  void fromProto(protocols::communication::Flags flags) override;
 
   std::optional<bool> robot_locked;
   std::optional<bool> critical_move;
