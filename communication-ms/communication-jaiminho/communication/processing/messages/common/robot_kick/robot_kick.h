fc2d3b53d0cfbb11f8d8912e01c717950f012723
ersaraujo <ersa@cin.ufpe.br>
2024-10-27 23:06:15 -0300

    Refactor RobotKick message implementation to cpp file

--- communication-ms/communication-jaiminho/communication/processing/messages/common/robot_kick/robot_kick.h
+++ communication-ms/communication-jaiminho/communication/processing/messages/common/robot_kick/robot_kick.h
@@ -16,24 +16,9 @@ class KickCommandMessage
                               std::optional<bool> charge_capacitor = std::nullopt,
                               std::optional<bool> is_bypass_ir = std::nullopt);
 
-  [[nodiscard]] protocols::common::RobotKick_KickCommand toProto() const override {
-    protocols::common::RobotKick_KickCommand kick_command;
-    kick_command.set_kick_strength(kick_strength.value_or(0));
-    kick_command.set_is_front(is_front.value_or(false));
-    kick_command.set_is_chip(is_chip.value_or(false));
-    kick_command.set_charge_capacitor(charge_capacitor.value_or(false));
-    kick_command.set_is_bypass_ir(is_bypass_ir.value_or(false));
+  [[nodiscard]] protocols::common::RobotKick_KickCommand toProto() const override;
 
-    return kick_command;
-  };
-
-  void fromProto(protocols::common::RobotKick_KickCommand kick_command) override {
-    kick_strength = kick_command.kick_strength();
-    is_front = kick_command.is_front();
-    is_chip = kick_command.is_chip();
-    charge_capacitor = kick_command.charge_capacitor();
-    is_bypass_ir = kick_command.is_bypass_ir();
-  };
+  void fromProto(protocols::common::RobotKick_KickCommand kick_command) override;
 
   std::optional<double> kick_strength;
   std::optional<bool> is_front;
@@ -46,13 +31,9 @@ class RobotKickMessage : public robocin::IProtoConvertible<protocols::common::Ro
  public:
   explicit RobotKickMessage(KickCommandMessage kick_command = KickCommandMessage{});
 
-  [[nodiscard]] protocols::common::RobotKick toProto() const override {
-    protocols::common::RobotKick robot_kick;
-    robot_kick.mutable_kick_command()->CopyFrom(kick_command.toProto());
-    return robot_kick;
-  };
+  [[nodiscard]] protocols::common::RobotKick toProto() const override;
 
-  void fromProto(protocols::common::RobotKick robot_kick) override {};
+  void fromProto(protocols::common::RobotKick robot_kick) override;
 
   KickCommandMessage kick_command;
 };
