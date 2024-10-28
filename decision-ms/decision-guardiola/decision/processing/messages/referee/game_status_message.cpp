1393fe3c32fee6c47c6c1293ad85d9d3ad95428a
fnalmeidap <fnap@cin.ufpe.br>
2024-10-28 11:51:50 -0300

    Add game status impl

--- decision-ms/decision-guardiola/decision/processing/messages/referee/game_status_message.cpp
+++ decision-ms/decision-guardiola/decision/processing/messages/referee/game_status_message.cpp
@@ -1,1 +1,79 @@
 #include "decision/processing/messages/referee/game_status_message.h"
+
+#include <utility>
+
+namespace decision {
+GameEventsProposalMessage::GameEventsProposalMessage(std::optional<std::string> proposal_id,
+                                                     std::vector<GameEventMessage> game_events,
+                                                     std::optional<bool> was_accepted) :
+    proposal_id(std::move(proposal_id)),
+    was_accepted(was_accepted) {};
+
+TeamMessage::TeamMessage(std::optional<std::string> name,
+                         std::optional<uint32_t> score,
+                         std::vector<RobotIdMessage> robot_ids,
+                         std::optional<RobotIdMessage> goalkeeper_id,
+                         std::optional<uint32_t> yellow_cards,
+                         std::vector<uint32_t> time_to_expire_active_yellow_cards,
+                         std::optional<uint32_t> red_cards,
+                         std::optional<uint32_t> timeouts_left,
+                         std::optional<google::protobuf::Duration> total_timeout_time_left,
+                         std::optional<uint32_t> total_fouls_committed,
+                         std::optional<uint32_t> consecutive_ball_placement_failures,
+                         std::optional<bool> is_ball_placement_enabled,
+                         std::optional<bool> has_ball_placement_failures_reached_maximum,
+                         std::optional<uint32_t> maximum_allowed_robots,
+                         std::optional<bool> is_robot_substitution_requested,
+                         std::optional<bool> is_robot_substitution_allowed,
+                         std::optional<uint32_t> robot_substitutions_left,
+                         std::optional<uint32_t> robot_substitution_time_left) :
+    name(std::move(name)),
+    score(score),
+    robot_ids(std::move(robot_ids)),
+    goalkeeper_id(std::move(goalkeeper_id)),
+    yellow_cards(yellow_cards),
+    time_to_expire_active_yellow_cards(std::move(time_to_expire_active_yellow_cards)),
+    red_cards(red_cards),
+    timeouts_left(timeouts_left),
+    total_timeout_time_left(std::move(total_timeout_time_left)),
+    total_fouls_committed(total_fouls_committed),
+    consecutive_ball_placement_failures(consecutive_ball_placement_failures),
+    is_ball_placement_enabled(is_ball_placement_enabled),
+    has_ball_placement_failures_reached_maximum(has_ball_placement_failures_reached_maximum),
+    maximum_allowed_robots(maximum_allowed_robots),
+    is_robot_substitution_requested(is_robot_substitution_requested),
+    is_robot_substitution_allowed(is_robot_substitution_allowed),
+    robot_substitutions_left(robot_substitutions_left),
+    robot_substitution_time_left(robot_substitution_time_left) {}
+
+GameStatusMessage::GameStatusMessage(
+    std::optional<std::string> source_id,
+    std::optional<std::string> description,
+    std::optional<google::protobuf::Timestamp> timestamp,
+    std::optional<MatchType> match_type,
+    std::optional<Team> home_team,
+    std::optional<Team> away_team,
+    std::optional<GameStage> game_stage,
+    std::optional<google::protobuf::Duration> game_stage_time_left,
+    std::optional<uint64_t> total_commands_issued,
+    std::optional<google::protobuf::Timestamp> command_issued_timestamp,
+    std::optional<GameCommandMessage> command,
+    std::optional<GameCommandMessage> next_command,
+    std::vector<GameEventMessage> game_events,
+    std::vector<GameEventsProposalMessage> game_events_proposals) :
+    source_id(std::move(source_id)),
+    description(std::move(description)),
+    timestamp(std::move(timestamp)),
+    match_type(match_type),
+    home_team(home_team),
+    away_team(away_team),
+    game_stage(game_stage),
+    game_stage_time_left(std::move(game_stage_time_left)),
+    total_commands_issued(total_commands_issued),
+    command_issued_timestamp(std::move(command_issued_timestamp)),
+    command(std::move(command)),
+    next_command(std::move(next_command)),
+    game_events(std::move(game_events)),
+    game_events_proposals(std::move(game_events_proposals)) {}
+
+} // namespace decision
--- decision-ms/decision-guardiola/decision/processing/messages/referee/game_status_message.h
+++ decision-ms/decision-guardiola/decision/processing/messages/referee/game_status_message.h
@@ -7,19 +7,32 @@
 #include "decision/processing/messages/iproto_convertible.h"
 
 #include <cstdint>
+#include <optional>
 #include <protocols/referee/game_status.pb.h>
 #include <utility>
 
 namespace decision {
 
-class GameEventsProposalMessage {
+class GameEventsProposalMessage
+    : public IProtoConvertible<protocols::referee::GameStatus::GameEventsProposal> {
  public:
   std::optional<std::string> proposal_id;
   std::vector<GameEventMessage> game_events;
   std::optional<bool> was_accepted;
+
+  explicit GameEventsProposalMessage(std::optional<std::string> proposal_id,
+                                     std::vector<GameEventMessage> game_events,
+                                     std::optional<bool> was_accepted);
+
+  [[nodiscard]] protocols::referee::GameStatus::GameEventsProposal toProto() const override {
+    return protocols::referee::GameStatus::GameEventsProposal{};
+  };
+
+  void
+  fromProto(const protocols::referee::GameStatus::GameEventsProposal& game_eventts_proposal_proto);
 };
 
-class TeamMessage {
+class TeamMessage : public IProtoConvertible<protocols::referee::GameStatus::Team> {
  public:
   std::optional<std::string> name;
   std::optional<uint32_t> score;
@@ -28,7 +41,7 @@ class TeamMessage {
   std::optional<RobotIdMessage> goalkeeper_id;
 
   std::optional<uint32_t> yellow_cards;
-  std::vector<google::protobuf::Duration> time_to_expire_active_yellow_cards;
+  std::vector<uint32_t> time_to_expire_active_yellow_cards;
 
   std::optional<uint32_t> red_cards;
 
@@ -47,21 +60,38 @@ class TeamMessage {
   std::optional<bool> is_robot_substitution_allowed;
 
   std::optional<uint32_t> robot_substitutions_left;
-  std::optional<google::protobuf::Duration> robot_substitution_time_left;
+  std::optional<uint32_t>
+      robot_substitution_time_left; // todo(fnap): discuss what to do with proto duration
+
+  explicit TeamMessage(std::optional<std::string> name = std::nullopt,
+                       std::optional<uint32_t> score = std::nullopt,
+                       std::vector<RobotIdMessage> robot_ids = {},
+                       std::optional<RobotIdMessage> goalkeeper_id = std::nullopt,
+                       std::optional<uint32_t> yellow_cards = std::nullopt,
+                       std::vector<uint32_t> time_to_expire_active_yellow_cards = {},
+                       std::optional<uint32_t> red_cards = std::nullopt,
+                       std::optional<uint32_t> timeouts_left = std::nullopt,
+                       std::optional<google::protobuf::Duration> total_timeout_time_left
+                       = std::nullopt,
+                       std::optional<uint32_t> total_fouls_committed = std::nullopt,
+                       std::optional<uint32_t> consecutive_ball_placement_failures = std::nullopt,
+                       std::optional<bool> is_ball_placement_enabled = std::nullopt,
+                       std::optional<bool> has_ball_placement_failures_reached_maximum
+                       = std::nullopt,
+                       std::optional<uint32_t> maximum_allowed_robots = std::nullopt,
+                       std::optional<bool> is_robot_substitution_requested = std::nullopt,
+                       std::optional<bool> is_robot_substitution_allowed = std::nullopt,
+                       std::optional<uint32_t> robot_substitutions_left = std::nullopt,
+                       std::optional<uint32_t> robot_substitution_time_left = std::nullopt);
+
+  [[nodiscard]] protocols::referee::GameStatus::Team toProto() const override {
+    return protocols::referee::GameStatus::Team{};
+  };
+
+  void fromProto(const protocols::referee::GameStatus::Team& team_proto);
 };
 
-class GameEventsProposal {
- public:
-  explicit GameEventsProposal(std::string proposal_id,
-                              const std::vector<GameEventMessage>& game_events,
-                              bool was_accepted);
-
-  std::string proposal_id;
-  std::vector<GameEventMessage> game_events;
-  bool was_accepted;
-};
-
-class GameStatusMessage {
+class GameStatusMessage : public IProtoConvertible<protocols::referee::GameStatus> {
  public:
   enum GameStage {
     GAME_STAGE_UNSPECIFIED = 0,
@@ -105,7 +135,30 @@ class GameStatusMessage {
   std::optional<GameCommandMessage> next_command;
 
   std::vector<GameEventMessage> game_events;
-  std::vector<GameEventsProposal> game_events_proposals;
+  std::vector<GameEventsProposalMessage> game_events_proposals;
+
+  explicit GameStatusMessage(std::optional<std::string> source_id = std::nullopt,
+                             std::optional<std::string> description = std::nullopt,
+                             std::optional<google::protobuf::Timestamp> timestamp = std::nullopt,
+                             std::optional<MatchType> match_type = std::nullopt,
+                             std::optional<Team> home_team = std::nullopt,
+                             std::optional<Team> away_team = std::nullopt,
+                             std::optional<GameStage> game_stage = std::nullopt,
+                             std::optional<google::protobuf::Duration> game_stage_time_left
+                             = std::nullopt,
+                             std::optional<uint64_t> total_commands_issued = std::nullopt,
+                             std::optional<google::protobuf::Timestamp> command_issued_timestamp
+                             = std::nullopt,
+                             std::optional<GameCommandMessage> command = std::nullopt,
+                             std::optional<GameCommandMessage> next_command = std::nullopt,
+                             std::vector<GameEventMessage> game_events = {},
+                             std::vector<GameEventsProposalMessage> game_events_proposals = {});
+
+  [[nodiscard]] protocols::referee::GameStatus toProto() const override {
+    return protocols::referee::GameStatus{};
+  };
+
+  void fromProto(const protocols::referee::GameStatus& game_status_proto);
 };
 } // namespace decision
 
