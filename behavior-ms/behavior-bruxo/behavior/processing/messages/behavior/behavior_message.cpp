dbc4d493514dd3799cfde6c13967320f1f7b4bb3
ersaraujo <ersa@cin.ufpe.br>
2024-10-30 17:39:38 -0300

    Refactor BehaviorMessage implementation to cpp file

--- behavior-ms/behavior-bruxo/behavior/processing/messages/behavior/behavior_message.cpp
+++ behavior-ms/behavior-bruxo/behavior/processing/messages/behavior/behavior_message.cpp
@@ -2,6 +2,7 @@
 
 namespace behavior {
 
+// OutputMessage
 OutputMessage::OutputMessage(RobotIdMessage robot_id,
                              MotionMessage motion,
                              PlanningMessage planning) :
@@ -9,7 +10,33 @@ OutputMessage::OutputMessage(RobotIdMessage robot_id,
     motion{std::move(motion)},
     planning{std::move(planning)} {}
 
-BehaviorMessage::BehaviorMessage(std::vector<OutputMessage> output) :
-    output{std::move(output)} {}
+protocols::behavior::unification::Output OutputMessage::toProto() const {
+  protocols::behavior::unification::Output output;
+  output.mutable_robot_id()->CopyFrom(robot_id.toProto());
+  output.mutable_motion()->CopyFrom(motion.toProto());
+  output.mutable_planning()->CopyFrom(planning.toProto());
+  return output;
+}
 
-}    // namespace behavior
+void OutputMessage::fromProto(const protocols::behavior::unification::Output& output) {
+  robot_id.fromProto(output.robot_id());
+  motion.fromProto(output.motion());
+  planning.fromProto(output.planning());
+}
+
+// BehaviorMessage
+BehaviorMessage::BehaviorMessage(std::vector<OutputMessage> output) : output{std::move(output)} {}
+
+protocols::behavior::unification::Behavior BehaviorMessage::toProto() const {
+  protocols::behavior::unification::Behavior behavior;
+  for (const auto& output_message : output) {
+    behavior.add_output()->CopyFrom(output_message.toProto());
+  }
+  return behavior;
+}
+
+void BehaviorMessage::fromProto(const protocols::behavior::unification::Behavior& behavior) {
+  // TODO: Implement this function
+}
+
+} // namespace behavior
--- behavior-ms/behavior-bruxo/behavior/processing/messages/behavior/behavior_message.h
+++ behavior-ms/behavior-bruxo/behavior/processing/messages/behavior/behavior_message.h
@@ -1,56 +1,52 @@
-#include "behavior/processing/messages/motion/motion_message.h"
 #include "behavior/processing/messages/common/robot_id/robot_id.h"
+#include "behavior/processing/messages/motion/motion_message.h"
 #include "behavior/processing/messages/planning/planning_message.h"
 
 #include <protocols/behavior/behavior_unification.pb.h>
-
 #include <robocin/output/log.h>
 #include <robocin/utility/iproto_convertible.h>
 
 namespace behavior {
 
+/**
+ * @class OutputMessage
+ * @brief A class that represents the output of the behavior module.
+ *
+ * This class is used to represent the output of the behavior module. It contains the robot id, the
+ * motion, and the planning.
+ */
+
 class OutputMessage : public robocin::IProtoConvertible<protocols::behavior::unification::Output> {
-  public:
-    OutputMessage(RobotIdMessage robot_id = RobotIdMessage{}, 
-                  MotionMessage motion = MotionMessage{},
-                  PlanningMessage planning = PlanningMessage{});
+ public:
+  OutputMessage(RobotIdMessage robot_id = RobotIdMessage{},
+                MotionMessage motion = MotionMessage{},
+                PlanningMessage planning = PlanningMessage{});
 
-    [[nodiscard]] protocols::behavior::unification::Output toProto() const override { 
-        protocols::behavior::unification::Output output;
-            output.mutable_robot_id()->CopyFrom(robot_id.toProto());
-            output.mutable_motion()->CopyFrom(motion.toProto());
-            output.mutable_planning()->CopyFrom(planning.toProto());
-        return output;
-    };
+  [[nodiscard]] protocols::behavior::unification::Output toProto() const override;
 
-    void fromProto(protocols::behavior::unification::Output output) override {
-        robot_id.fromProto(output.robot_id());
-        motion.fromProto(output.motion());
-        planning.fromProto(output.planning());
-    };
+  void fromProto(const protocols::behavior::unification::Output& output) override;
 
-    RobotIdMessage robot_id;
-    MotionMessage motion;
-    PlanningMessage planning;
+  RobotIdMessage robot_id;
+  MotionMessage motion;
+  PlanningMessage planning;
 };
 
-class BehaviorMessage : public robocin::IProtoConvertible<protocols::behavior::unification::Behavior> {
-  public:
-    BehaviorMessage(std::vector<OutputMessage> output = std::vector<OutputMessage>{});
+/**
+ * @class BehaviorMessage
+ * @brief A class that represents the behavior message.
+ *
+ * This class is used to represent the behavior message. It contains a vector of output messages.
+ */
+class BehaviorMessage
+    : public robocin::IProtoConvertible<protocols::behavior::unification::Behavior> {
+ public:
+  BehaviorMessage(std::vector<OutputMessage> output = std::vector<OutputMessage>{});
 
-    [[nodiscard]] protocols::behavior::unification::Behavior toProto() const override {
-        protocols::behavior::unification::Behavior behavior;
-        for (const auto& output : output) {
-            behavior.add_output()->CopyFrom(output.toProto());
-        }
-        return behavior;
-    };
+  [[nodiscard]] protocols::behavior::unification::Behavior toProto() const override;
 
-    void fromProto(protocols::behavior::unification::Behavior behavior) override {
-        // Implementação específica para converter de proto para BehaviorMessage
-    };
+  void fromProto(const protocols::behavior::unification::Behavior& behavior) override;
 
-    std::vector<OutputMessage> output;
+  std::vector<OutputMessage> output;
 };
 
-}    // namespace behavior
+} // namespace behavior
