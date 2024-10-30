c92d34b86afabfc1d7774ba56e7b6af83292282e
ersaraujo <ersa@cin.ufpe.br>
2024-10-30 17:39:12 -0300

    Refactor RobotIdMessage implementation to cpp file

--- behavior-ms/behavior-bruxo/behavior/processing/messages/common/robot_id/robot_id.h
+++ behavior-ms/behavior-bruxo/behavior/processing/messages/common/robot_id/robot_id.h
@@ -1,39 +1,34 @@
 #ifndef BEHAVIOR_PROCESSING_MESSAGES_COMMON_ROBOT_ID_H
 #define BEHAVIOR_PROCESSING_MESSAGES_COMMON_ROBOT_ID_H
 
-#include <robocin/utility/iproto_convertible.h>
-
 #include <protocols/common/robot_id.pb.h>
+#include <robocin/utility/iproto_convertible.h>
 
 namespace behavior {
 
+/**
+ * @class RobotIdMessage
+ * @brief A class that represents the robot id.
+ *
+ * This class is used to represent the robot id. It contains the color and the number of the robot.
+ */
 class RobotIdMessage : public robocin::IProtoConvertible<protocols::common::RobotId> {
-    enum Color {
-        COLOR_UNSPECIFIED = 0,
-        COLOR_YELLOW = 1,
-        COLOR_BLUE = 2,
-    };
+  enum Color {
+    COLOR_UNSPECIFIED = 0,
+    COLOR_YELLOW = 1,
+    COLOR_BLUE = 2,
+  };
 
-  public:
-    explicit RobotIdMessage(std::optional<Color> color = std::nullopt,
-                            std::optional<uint32_t> number = 0);
+ public:
+  explicit RobotIdMessage(std::optional<Color> color = std::nullopt,
+                          std::optional<uint32_t> number = 0);
 
-    [[nodiscard]] protocols::common::RobotId toProto() const override {
-        protocols::common::RobotId robot_id;
-        robot_id.set_color(static_cast<::protocols::common::RobotId_Color>(color.value_or(Color::COLOR_UNSPECIFIED)));
-        robot_id.set_number(number.value_or(0));
-    
-        return robot_id;
-    };
+  [[nodiscard]] protocols::common::RobotId toProto() const override;
 
-    void fromProto(protocols::common::RobotId robot_id) override {
-        color = static_cast<Color>(robot_id.color());
-        number = robot_id.number();
-    };
+  void fromProto(const protocols::common::RobotId& robot_id) override;
 
-    std::optional<Color> color;
-    std::optional<uint32_t> number;
-
+  std::optional<Color> color;
+  std::optional<uint32_t> number;
 };
 
 } // namespace behavior
