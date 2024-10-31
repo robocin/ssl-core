3dc5678f246b7e8c45b741b956e9c11b508d4790
Marcela <maa5@cin.ufpe.br>
2024-10-31 17:40:02 -0300

    refactor: uses rc namespace

--- navigation-ms/navigation-luffy/navigation/processing/motion_parser/motion_parser.cpp
+++ navigation-ms/navigation-luffy/navigation/processing/motion_parser/motion_parser.cpp
@@ -1,5 +1,4 @@
 #include "navigation/processing/motion_parser/motion_parser.h"
-#include "motion_parser.h"
 
 #include <cstdlib>
 #include <navigation/parameters/parameters.h>
@@ -11,10 +10,21 @@
 
 namespace navigation {
 
+namespace {
+namespace rc {
+using ::protocols::behavior::GoToPoint;
+using ::protocols::behavior::PathNode;
+using ::protocols::behavior::PrecisionToTarget;
+using ::protocols::behavior::RotateInPoint;
+using ::protocols::behavior::RotateOnSelf;
+using ::protocols::common::Point2Df;
+using ::protocols::perception::Robot;
+} // namespace rc
+} // namespace
+
 MotionParser::MotionParser() {}
 
-RobotMove MotionParser::fromGoToPoint(const ::protocols::behavior::GoToPoint& go_to_point,
-                                      const ::protocols::perception::Robot& robot) {
+RobotMove MotionParser::fromGoToPoint(const rc::GoToPoint& go_to_point, const rc::Robot& robot) {
 
   robocin::Point2Dd s0 = robocin::Point2Dd(robot.position().x(), robot.position().y());
   robocin::Point2Dd s = robocin::Point2Dd(go_to_point.target().x(), go_to_point.target().y());
@@ -32,10 +42,9 @@ RobotMove MotionParser::fromGoToPoint(const ::protocols::behavior::GoToPoint& go
                    minVelocity);
   }
 
-  double tolerance_to_target
-      = go_to_point.precision_to_target() == protocols::behavior::PrecisionToTarget::High ?
-            SMALL_TOLERANCE_TO_DESIRED_POSITION_M :
-            DEFAULT_TOLERANCE_TO_DESIRED_POSITION_M;
+  double tolerance_to_target = go_to_point.precision_to_target() == rc::PrecisionToTarget::High ?
+                                   SMALL_TOLERANCE_TO_DESIRED_POSITION_M :
+                                   DEFAULT_TOLERANCE_TO_DESIRED_POSITION_M;
 
   const double delta_theta
       = ::robocin::smallestAngleDiff<double>(robot.angle(), go_to_point.target_angle());
@@ -48,8 +57,8 @@ RobotMove MotionParser::fromGoToPoint(const ::protocols::behavior::GoToPoint& go
     double acc_prop = moving_profile::ROBOT_DEFAULT_LINEAR_ACCELERATION;
     auto v0 = robocin::Point2Dd(robot.velocity().x(), robot.velocity().y()) / M_to_MM;
     auto v = robocin::Point2D<double>::fromPolar(maxVelocity, theta);
-    const double v0_decay = std::abs(v.angleTo(v0)) > PI/3 ? ROBOT_VEL_BREAK_DECAY_FACTOR :
-                                                           ROBOT_VEL_FAVORABLE_DECAY_FACTOR;
+    const double v0_decay = std::abs(v.angleTo(v0)) > PI / 3 ? ROBOT_VEL_BREAK_DECAY_FACTOR :
+                                                               ROBOT_VEL_FAVORABLE_DECAY_FACTOR;
 
     // v = v0 + a*t
     v0 = v0 - (v0 * v0_decay) * CYCLE_STEP;
@@ -78,16 +87,16 @@ RobotMove MotionParser::fromGoToPoint(const ::protocols::behavior::GoToPoint& go
 
     // debug
 
-    ::protocols::behavior::PathNode new_path_node;
+    rc::PathNode new_path_node;
 
     new_path_node.set_time(0);
 
-    ::protocols::common::Point2Df* velocity_ptr = nullptr;
+    rc::Point2Df* velocity_ptr = nullptr;
     velocity_ptr->set_x(0);
     velocity_ptr->set_y(0);
     new_path_node.set_allocated_velocity(velocity_ptr);
 
-    ::protocols::common::Point2Df* position_ptr = nullptr;
+    rc::Point2Df* position_ptr = nullptr;
     position_ptr->set_x(go_to_point.target().x());
     position_ptr->set_y(go_to_point.target().y());
     new_path_node.set_allocated_position(position_ptr);
@@ -98,16 +107,15 @@ RobotMove MotionParser::fromGoToPoint(const ::protocols::behavior::GoToPoint& go
   return RobotMove{{0, 0}, std::clamp(kp * delta_theta, -maxAngularVel, maxAngularVel)};
 }
 
-RobotMove
-MotionParser::fromRotateInPoint(const ::protocols::behavior::RotateInPoint& rotate_in_point,
-                                const ::protocols::perception::Robot& robot) {
+RobotMove MotionParser::fromRotateInPoint(const rc::RotateInPoint& rotate_in_point,
+                                          const rc::Robot& robot) {
 
   // PROCESSAMENTO DO ROTATEINPOINT
   return RobotMove{};
 }
 
-RobotMove MotionParser::fromRotateOnSelf(const ::protocols::behavior::RotateOnSelf& rotate_on_self,
-                                         const ::protocols::perception::Robot& robot) {
+RobotMove MotionParser::fromRotateOnSelf(const rc::RotateOnSelf& rotate_on_self,
+                                         const rc::Robot& robot) {
 
   // PROCESSAMENTO DO ROTATEINPOINT
   return RobotMove{};
