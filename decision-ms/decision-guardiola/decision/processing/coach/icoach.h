4bbd0deee7c1406f14efb24ae5783146d0d96c10
Matheus Paixão Gumercindo dos Santos <61971668+MatheusPaixaoG@users.noreply.github.com>
2024-11-01 16:44:54 -0300

    Add initial decision structure (#68)
    This PR adds the initial interfaces that will be used to structure the
    decision microservice. Their intention is to facilitate further
    development and expansion of the decision processing, with each
    calculation being done in its own evaluator, and with the evaluators
    being processed in a class that implements ICoach.

---
+++ decision-ms/decision-guardiola/decision/processing/coach/icoach.h
@@ -0,0 +1,31 @@
+#ifndef DECISION_PROCESSING_COACH_ICOACH_H
+#define DECISION_PROCESSING_COACH_ICOACH_H
+
+#include "decision/processing/coach/tactical_plan/tactical_plan.h"
+#include "decision/processing/evaluators/ievaluator.h"
+
+#include <memory>
+#include <vector>
+
+namespace decision {
+
+class ICoach {
+ public:
+  ICoach() = default;
+
+  ICoach(const ICoach&) = delete;
+  ICoach& operator=(const ICoach&) = delete;
+
+  ICoach(ICoach&&) = default;
+  ICoach& operator=(ICoach&&) = default;
+
+  virtual ~ICoach() = default;
+
+  virtual void process() = 0;
+  virtual void reset() = 0;
+  virtual TacticalPlan getTacticalPlan() = 0;
+};
+
+} // namespace decision
+
+#endif /* DECISION_PROCESSING_COACH_ICOACH_H */
---
+++ decision-ms/decision-guardiola/decision/processing/evaluators/ievaluator.h
@@ -0,0 +1,26 @@
+#ifndef DECISION_PROCESSING_EVALUATORS_IEVALUATOR_H
+#define DECISION_PROCESSING_EVALUATORS_IEVALUATOR_H
+
+#include "decision/processing/evaluators/evaluator_result.h"
+namespace decision {
+
+class IEvaluator {
+ public:
+  IEvaluator() = default;
+
+  IEvaluator(const IEvaluator&) = delete;
+  IEvaluator& operator=(const IEvaluator&) = delete;
+
+  IEvaluator(IEvaluator&&) = default;
+  IEvaluator& operator=(IEvaluator&&) = default;
+
+  virtual ~IEvaluator() = default;
+
+  virtual void process() = 0;
+  virtual void reset() = 0;
+  virtual EvaluatorResult& getResult() = 0;
+};
+
+} // namespace decision
+
+#endif /* DECISION_PROCESSING_EVALUATORS_IEVALUATOR_H */
