d83763870ea6092d5e800a48f9f69fd337881136
Elisson Araújo <44985807+ersaraujo@users.noreply.github.com>
2024-11-03 13:38:44 -0300

    Add referee service subscription (#81)
    Add referee as dependency.

--- behavior-ms/behavior-bruxo/behavior/messaging/receiver/payload.cpp
+++ behavior-ms/behavior-bruxo/behavior/messaging/receiver/payload.cpp
@@ -1,5 +1,7 @@
 #include "behavior/messaging/receiver/payload.h"
 
+#include <protocols/referee/game_status.pb.h>
+
 namespace behavior {
 namespace {
 
@@ -7,18 +9,24 @@ namespace rc {
 
 using ::protocols::decision::Decision;
 using ::protocols::perception::Detection;
+using ::protocols::referee::GameStatus;
 
 } // namespace rc
 
 } // namespace
 
-Payload::Payload(std::vector<rc::Detection> detections, std::vector<rc::Decision> decisions) :
+Payload::Payload(std::vector<rc::Detection> detections,
+                 std::vector<rc::Decision> decisions,
+                 std::vector<rc::GameStatus> game_status) :
     detections_{std::move(detections)},
-    decisions_{std::move(decisions)} {}
+    decisions_{std::move(decisions)},
+    game_status_{std::move(game_status)} {}
 
 std::span<const rc::Detection> Payload::getDetectionMessages() const { return detections_; }
 
 std::span<const rc::Decision> Payload::getDecisionMessages() const { return decisions_; }
+
+std::span<const rc::GameStatus> Payload::getGameStatusMessages() const { return game_status_; }
 
 bool Payload::empty() const { return detections_.empty() and decisions_.empty(); }
 
