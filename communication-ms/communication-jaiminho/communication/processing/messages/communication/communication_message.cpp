0a7e384381404beafb48c4ab1c2a7f480e27d76d
ersaraujo <ersa@cin.ufpe.br>
2024-11-06 18:19:10 -0300

    add proto to send robot command

--- communication-ms/communication-jaiminho/communication/processing/messages/communication/communication_message.cpp
+++ communication-ms/communication-jaiminho/communication/processing/messages/communication/communication_message.cpp
@@ -2,6 +2,7 @@
 
 namespace communication {
 
+////////////// CommandMessage //////////////
 CommandMessage::CommandMessage(MessageTypeMessage message_type,
                                RobotIdMessage robot_id,
                                RobotVelocityMessage robot_velocity,
@@ -27,7 +28,7 @@ protocols::communication::Command CommandMessage::toProto() const {
   return command;
 }
 
-void CommandMessage::fromProto(const protocols::communication::Command command) {
+void CommandMessage::fromProto(const protocols::communication::Command& command) {
   message_type.fromProto(command.msg_type());
   robot_id.fromProto(command.robot_id());
   robot_velocity.fromProto(command.robot_velocity());
@@ -36,6 +37,7 @@ void CommandMessage::fromProto(const protocols::communication::Command command) 
   robot_flags.fromProto(command.robot_flags());
 }
 
+////////////// OutputMessage //////////////
 OutputMessage::OutputMessage(CommandMessage commands) : command(std::move(commands)) {}
 
 protocols::communication::Output OutputMessage::toProto() const {
@@ -45,10 +47,11 @@ protocols::communication::Output OutputMessage::toProto() const {
   return output;
 }
 
-void OutputMessage::fromProto(const protocols::communication::Output output) {
+void OutputMessage::fromProto(const protocols::communication::Output& output) {
   command.fromProto(output.command());
 }
 
+////////////// CommunicationMessage //////////////
 CommunicationMessage::CommunicationMessage(std::vector<OutputMessage> output_messages) :
     output_messages(std::move(output_messages)) {}
 
@@ -61,5 +64,73 @@ protocols::communication::RobotInfo CommunicationMessage::toProto() const {
   return robot_info;
 }
 
-void CommunicationMessage::fromProto(const protocols::communication::RobotInfo robot_info) {}
+void CommunicationMessage::fromProto(const protocols::communication::RobotInfo& robot_info) {}
+
+////////////// CommandOutputMessage //////////////
+CommandOutputMessage::CommandOutputMessage(uint32_t id,
+                                           double vx,
+                                           double vy,
+                                           double vw,
+                                           bool front,
+                                           bool chip,
+                                           bool charge,
+                                           double kick_strength,
+                                           bool dribbler,
+                                           double dribbler_speed) :
+    id(id),
+    vx(vx),
+    vy(vy),
+    vw(vw),
+    front(front),
+    chip(chip),
+    charge(charge),
+    kick_strength(kick_strength),
+    dribbler(dribbler),
+    dribbler_speed(dribbler_speed) {}
+
+protocols::communication::CommandOutput CommandOutputMessage::toProto() const {
+  protocols::communication::CommandOutput command_output;
+  command_output.set_id(id);
+  command_output.set_vx(vx);
+  command_output.set_vy(vy);
+  command_output.set_vw(vw);
+  command_output.set_front(front);
+  command_output.set_chip(chip);
+  command_output.set_charge(charge);
+  command_output.set_kick_strength(kick_strength);
+  command_output.set_dribbler(dribbler);
+  command_output.set_dribbler_speed(dribbler_speed);
+
+  return command_output;
+}
+
+void CommandOutputMessage::fromProto(
+    const protocols::communication::CommandOutput& command_output) {
+  id = command_output.id();
+  vx = command_output.vx();
+  vy = command_output.vy();
+  vw = command_output.vw();
+  front = command_output.front();
+  chip = command_output.chip();
+  charge = command_output.charge();
+  kick_strength = command_output.kick_strength();
+  dribbler = command_output.dribbler();
+  dribbler_speed = command_output.dribbler_speed();
+}
+
+////////////// RobotCommandMessage //////////////
+RobotCommandMessage::RobotCommandMessage(std::vector<CommandOutputMessage> commands) :
+    commands(std::move(commands)) {}
+
+protocols::communication::RobotCommand RobotCommandMessage::toProto() const {
+  protocols::communication::RobotCommand robot_command;
+  for (const auto& command : commands) {
+    robot_command.add_robot_command()->CopyFrom(command.toProto());
+  }
+
+  return robot_command;
+}
+
+void RobotCommandMessage::fromProto(const protocols::communication::RobotCommand& robot_command) {}
+
 } // namespace communication
--- communication-ms/communication-jaiminho/communication/processing/messages/communication/communication_message.h
+++ communication-ms/communication-jaiminho/communication/processing/messages/communication/communication_message.h
@@ -26,7 +26,7 @@ class CommandMessage : public robocin::IProtoConvertible<protocols::communicatio
 
   [[nodiscard]] protocols::communication::Command toProto() const override;
 
-  void fromProto(protocols::communication::Command command) override;
+  void fromProto(const protocols::communication::Command& command) override;
 
   MessageTypeMessage message_type;
   RobotIdMessage robot_id;
@@ -42,21 +42,79 @@ class OutputMessage : public robocin::IProtoConvertible<protocols::communication
 
   [[nodiscard]] protocols::communication::Output toProto() const override;
 
-  void fromProto(protocols::communication::Output output) override;
+  void fromProto(const protocols::communication::Output& output) override;
 
   CommandMessage command;
+};
+
+class CommandOutputMessage
+    : public robocin::IProtoConvertible<protocols::communication::CommandOutput> {
+ public:
+  // PROTO BASE:
+  //  message CommandOutput {
+  //   uint32 id = 1;
+  //   double vx = 2;
+  //   double vy = 3;
+  //   double vw = 4;
+  //   bool front = 5;
+  //   bool chip = 6;
+  //   bool charge = 7;
+  //   double kick_strength = 8;
+  //   bool dribbler = 9;
+  //   double dribbler_speed = 10;
+  // }
+
+  explicit CommandOutputMessage(uint32_t id = 0,
+                                double vx = 0,
+                                double vy = 0,
+                                double vw = 0,
+                                bool front = false,
+                                bool chip = false,
+                                bool charge = false,
+                                double kick_strength = 0,
+                                bool dribbler = false,
+                                double dribbler_speed = 0);
+
+  [[nodiscard]] protocols::communication::CommandOutput toProto() const override;
+
+  void fromProto(const protocols::communication::CommandOutput& command_output) override;
+
+  uint32_t id;
+  double vx;
+  double vy;
+  double vw;
+  bool front;
+  bool chip;
+  bool charge;
+  double kick_strength;
+  bool dribbler;
+  double dribbler_speed;
 };
 
 class CommunicationMessage
     : public robocin::IProtoConvertible<protocols::communication::RobotInfo> {
  public:
-  CommunicationMessage(std::vector<OutputMessage> output_messages = std::vector<OutputMessage>{});
+  explicit CommunicationMessage(std::vector<OutputMessage> output_messages
+                                = std::vector<OutputMessage>{});
 
   [[nodiscard]] protocols::communication::RobotInfo toProto() const override;
 
-  void fromProto(protocols::communication::RobotInfo robot_info) override;
+  void fromProto(const protocols::communication::RobotInfo& robot_info) override;
 
   std::vector<OutputMessage> output_messages;
+};
+
+class RobotCommandMessage
+    : public robocin::IProtoConvertible<protocols::communication::RobotCommand> {
+ public:
+  explicit RobotCommandMessage(std::vector<CommandOutputMessage> commands
+                               = std::vector<CommandOutputMessage>{});
+
+  [[nodiscard]] protocols::communication::RobotCommand toProto() const override;
+
+  void fromProto(const protocols::communication::RobotCommand& robot_command) override;
+
+  std::vector<CommandOutputMessage> commands;
 };
 
 } // namespace communication
