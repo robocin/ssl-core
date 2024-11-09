f74a8780376416c25c929a31258bc984c38e4475
matheuslafayette <matheuslafayett@gmail.com>
2024-11-09 19:19:40 -0300

    feat: use ball position from world

--- behavior-ms/behavior-bruxo/behavior/processing/state_machine/goalkeeper_take_ball_away/states/go_to_ball.cpp
+++ behavior-ms/behavior-bruxo/behavior/processing/state_machine/goalkeeper_take_ball_away/states/go_to_ball.cpp
@@ -10,14 +10,19 @@ GoToBall::GoToBall() = default;
 OutputMessage GoToBall::exec(const World& world) {
   robocin::ilog("Exec GoToBall state");
 
+  robocin::Point2Df ball_position
+      = robocin::Point2Df{world.ball.position->x, world.ball.position->y};
+  // TODO(mlv): get ally id
+  const int ally_id = 0;
+
   bool is_ally_looking_to_target_and_ball
       = behavior::AllyAnalyzer::isAllyLookingToTargetAndBall(world,
-                                                             0,
-                                                             robocin::Point2Df(0, 0),
+                                                             ally_id,
+                                                             ball_position,
                                                              approach_angle_threshold);
 
   bool is_ball_in_range_to_kick
-      = behavior::AllyAnalyzer::isBallInRangeToKick(world, 0, distance_to_consider_kick);
+      = behavior::AllyAnalyzer::isBallInRangeToKick(world, ally_id, distance_to_consider_kick);
 
   if (is_ally_looking_to_target_and_ball && is_ball_in_range_to_kick) {
     state_machine_->transitionTo(new KickBall);
--- behavior-ms/behavior-bruxo/behavior/processing/state_machine/goalkeeper_take_ball_away/states/kick_ball.cpp
+++ behavior-ms/behavior-bruxo/behavior/processing/state_machine/goalkeeper_take_ball_away/states/kick_ball.cpp
@@ -1,5 +1,7 @@
 #include "behavior/processing/state_machine/goalkeeper_take_ball_away/states/kick_ball.h"
 
+#include "behavior/processing/state_machine/goalkeeper_take_ball_away/states/go_to_ball.h"
+
 #include <cstring>
 #include <robocin/geometry/point2d.h>
 #include <stdio.h>
@@ -11,17 +13,22 @@ KickBall::KickBall() = default;
 OutputMessage KickBall::exec(const World& world) {
   robocin::ilog("Exec KickBall state");
 
+  robocin::Point2Df ball_position
+      = robocin::Point2Df{world.ball.position->x, world.ball.position->y};
+  // TODO(mlv): get ally id
+  const int ally_id = 0;
+
   bool is_ally_looking_to_target_and_ball
       = behavior::AllyAnalyzer::isAllyLookingToTargetAndBall(world,
                                                              0,
-                                                             robocin::Point2Df(0, 0),
+                                                             ball_position,
                                                              approach_angle_threshold);
 
   bool is_ball_in_range_to_kick
       = behavior::AllyAnalyzer::isBallInRangeToKick(world, 0, distance_to_consider_kick);
 
   if (!is_ally_looking_to_target_and_ball || !is_ball_in_range_to_kick) {
-    state_machine_->transitionTo(new GoToSafePosition);
+    state_machine_->transitionTo(new GoToBall);
   }
 
   return makeKickBallOutput(world);
