a251d2a4c4d9154bf71de7cc2de2d2116cf2c392
matheuslafayette <matheuslafayett@gmail.com>
2024-11-10 19:11:50 -0300

    feat: return copy of ally

--- behavior-ms/behavior-bruxo/behavior/processing/state_machine/goalkeeper_take_ball_away/common/goalkeeper_take_ball_away_common.cpp
+++ behavior-ms/behavior-bruxo/behavior/processing/state_machine/goalkeeper_take_ball_away/common/goalkeeper_take_ball_away_common.cpp
@@ -5,8 +5,17 @@ namespace behavior {
 std::optional<RobotMessage> GoalkeeperCommon::getAlly(const World& world, int id) {
   for (const auto& ally : world.allies) {
     if (ally.robot_id.value().number == id) {
-      RobotMessage ally = std::move(ally);
-      return ally;
+      return RobotMessage{
+          ally.confidence.value(),
+          RobotIdMessage{ally.robot_id->color.value(), ally.robot_id->number.value()},
+          ally.position.value(),
+          ally.angle.value(),
+          ally.velocity.value(),
+          ally.angular_velocity.value(),
+          ally.radius.value(),
+          ally.height.value(),
+          ally.dribbler_width.value(),
+          std::nullopt /* Feedback */};
     }
   }
   return std::nullopt;
