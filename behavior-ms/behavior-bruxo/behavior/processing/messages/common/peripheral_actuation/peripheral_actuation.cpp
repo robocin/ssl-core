1ce8fbd08bfaf10def39a0b041d8d966c777676a
Felipe Nunes <fnap@cin.ufpe.br>
2024-11-10 19:21:34 -0300

    Add peripheral actuation to motion (#109)
    Add peripheral actuation message to Motion with kick command.
    
    ---------
    
    Co-authored-by: ersaraujo <ersa@cin.ufpe.br>

--- behavior-ms/behavior-bruxo/behavior/processing/messages/common/peripheral_actuation/peripheral_actuation.cpp
+++ behavior-ms/behavior-bruxo/behavior/processing/messages/common/peripheral_actuation/peripheral_actuation.cpp
@@ -1,1 +1,15 @@
 #include "behavior/processing/messages/common/peripheral_actuation/peripheral_actuation.h"
+
+#include "behavior/processing/messages/common/robot_kick/kick_command.h"
+
+namespace behavior {
+PeripheralActuationMessage::PeripheralActuationMessage(KickCommandMessage kick_command) :
+    kick_command(std::move(kick_command)) {};
+
+void PeripheralActuationMessage::fromProto(
+    const protocols::common::PeripheralActuation& peripheral_actuation_proto) {};
+
+protocols::common::PeripheralActuation PeripheralActuationMessage::toProto() const {
+  return protocols::common::PeripheralActuation{};
+};
+} // namespace behavior
